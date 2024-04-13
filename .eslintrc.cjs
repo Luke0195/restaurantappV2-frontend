181db48d9f9f45ad24527b19091265496ac40855
Lucas Santos <lucas201273@gmail.com>
2024-04-13 11:51:40 -0300

    fix: disable eslint rule on cypress.config.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/no-unused-vars': 0,
     'sort-imports': [
       'error',
       {
