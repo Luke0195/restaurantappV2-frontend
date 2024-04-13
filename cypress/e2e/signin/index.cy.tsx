155ca7ef1cf681ae9c83d8a5563543380eaa26c9
Lucas Santos <lucas201273@gmail.com>
2024-04-13 11:45:23 -0300

    test: ensure SignIn form navigate to home page when valid data is provided

--- cypress/e2e/signin/index.cy.tsx
+++ cypress/e2e/signin/index.cy.tsx
@@ -9,4 +9,11 @@ describe('SignIn Component ', () => {
     cy.get('input[name="password"]').should('have.value', '')
     cy.get('button').should('not.be.enabled')
   })
+
+  it('Should navigate to home page when valid data is provided', () => {
+    cy.get('input[name="email"').type('any@mail.com')
+    cy.get('input[name="password"').type('123')
+    cy.get('button').should('be.enabled')
+    cy.visit('/home')
+  })
 })
